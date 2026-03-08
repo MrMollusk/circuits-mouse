27065e94de766db984e740f9c5d93e579e888f88
Seán Reilly <reillys9@tcd.ie>
2026-03-08 17:01:11 +0000

    Integrate Kalman filter for sensor data processing

--- esp32-main.ino
+++ esp32-main.ino
@@ -1,8 +1,28 @@
 #include <Wire.h>
 #include <Arduino.h>
 #include <BleMouse.h>
+#include <BasicLinearAlgebra.h>
+#include <Kalman.h>
+
+using namespace BLA;
 
 byte Version[3];
+
+#define Nstate 2
+#define Nobs 2
+
+#define n_p 0.027225
+#define n_a 0.027225
+
+// model std (1/inertia)
+#define m_p 0.1
+#define m_s 0.1
+
+unsigned long T;
+float DT;
+
+BLA::Matrix<Nobs> obs;
+KALMAN<Nstate, Nobs> K;
 
 //PINS
 const int EncoderEight = 16;
@@ -15,9 +35,11 @@ const int RIGHT_CLICK = 17;
 
 //Global Variables
 byte range = 0x00;
-float divi = 16;
+float divi = 8;
 float rotationVal;
 int pastEncoderValue = 0;
+bool leftMouseCurrentlyBeingPressed = false;
+bool rightMouseCurrentlyBeingPressed = false;
 BleMouse bleMouse("AccelMouse", "ESP32", 100);
 
 void setup() {
@@ -41,7 +63,50 @@ void setup() {
   Wire.write(0x05);
   Wire.endTransmission();
 
+  /*
+  K.F tells us the following:
+
+  K.F = {How much the current roll angle depends on the previous roll value, How much the previous roll value depends on the pitch
+        How much the current pitch depends on the previous roll, How much the current pitch depends on the previous roll}
+  
+  */
+   K.F = {1.0, 0.0,
+		 0.0, 1.0}; 
+
+  /*
+  K.H tells us the following:
+
+  K.H = {How the current roll measurement corresponds to the roll state (state as in what the sensor is returning), How much the roll measurement currently depends on the pitch state,
+          How much the current pitch depends on the current roll state, How much the current pitch depends on the current pitch state}
+  */
+  K.H = {1.0, 0.0,
+         0.0, 1.0};
+
+/*
+K.R tells us the following values:
+
+K.H = {Measurement of the variance in the roll angle, The covariance of roll and pitch (should be zero as they are independent)
+        Covariance of pitch and roll (should be the same as previous value), Variance of pitch}
+*/
+  K.R = {n_p*n_p,   0.0,
+           0.0, n_a*n_a};
+
+
+  /*
+  K.Q differs from K.R by measuring the variance of the real, actual measurements, rather than measuring the variance of K.R
+
+  K.Q = {Variance of Random Roll Angle change between updates, How random pitch motion affects roll, 
+          How random rol motiona affects pitch prediction, Variance of random pitch angle change between updates}
+
+  */
+  K.Q = {m_p*m_p,     0.0, 
+             0.0, m_s*m_s
+  };
+
+
   bleMouse.begin();   // pair from PC Bluetooth settings
+
+  T = millis();
 }
 
 static inline int8_t clamp127(int v) { //This function just prevents the values from the accelerometer going over a certain value
@@ -87,15 +152,16 @@ void mouseFunction(float xVal, float yVal, int scroll) {
   int dy = (int)(yVal * 40.0f);
 
   // deadzone to stop jitter
-  if (abs(dx) < 10) dx = 0;
-  if (abs(dy) < 10) dy = 0;
+  if (abs(dx) < 1) dx = 0;
+  if (abs(dy) < 1) dy = 0;
 
   bleMouse.move(clamp127(dy), clamp127(dx), scroll);
 }
 
-void RotationSensor() {
+void RotationSensor(float& x, float& y) {
   rotationVal = analogRead(SCROLL);
-  divi = 2.0f + (7.0f/4095.0f)*rotationVal; //divi is used to change the sensitivity of the system
+  x = (x*0.25f/4095.0f)*rotationVal;
+  y = (y*0.25f/4095.0f)*rotationVal;
 }
 
 void printInformation(bool leftClickPressed, bool rightClickPressed, float x, float y, float z){
@@ -117,15 +183,25 @@ void printInformation(bool leftClickPressed, bool rightClickPressed, float x, fl
 }
 
 void buttonFunction(bool &leftClickPressed, bool &rightClickPressed){
+  if(!bleMouse.isConnected()) return;
+
   leftClickPressed = digitalRead(LEFT_CLICK);
   rightClickPressed = digitalRead(RIGHT_CLICK);
 
-  if(!leftClickPressed){ //Using NOTleftClickPressed because using INPUT_PULLUP
+  if(!leftClickPressed && !leftMouseCurrentlyBeingPressed){ //Using NOTleftClickPressed because using INPUT_PULLUP
     bleMouse.click(MOUSE_LEFT);
+    leftMouseCurrentlyBeingPressed = true;
+  }
+  else if(leftClickPressed){
+    leftMouseCurrentlyBeingPressed = false;
   }
   
-  if(!rightClickPressed){ //Using NOTleftClickPressed because using INPUT_PULLUP
+  if(!rightClickPressed && !rightMouseCurrentlyBeingPressed){ //Using NOTleftClickPressed because using INPUT_PULLUP
     bleMouse.click(MOUSE_RIGHT);
+    rightMouseCurrentlyBeingPressed = true;
+  }
+  else if(rightClickPressed){
+    rightMouseCurrentlyBeingPressed = false;
   }
 }
 
@@ -141,17 +217,83 @@ void accelerometerFunction(float &x, float &y, float &z){
   z = (float)z_data / divi;
 }
 
+float xVelocity(float x, float prevX, float prevT){
+  return (x-prevX)/(T - prevT);
+}
+
+float yVelocity(float y, float prevY, float prevT){
+  if(T == prevT){
+    return 0;
+  }
+  return (y-prevY)/(T - prevT);
+}
+
+
+float curvedResponse(float input) {
+  float scalingFactor;
+
+  if(input >= 0){
+    scalingFactor = 0.5;
+  }
+  else{
+    scalingFactor = -0.5;
+  }
+  
+  float a = fabs(input);
+
+  if (a < 0.05f) return 0.0f;   // deadzone
+
+  return scalingFactor*(exp(1.25f * a) - 1.0f);
+}
+
 void loop() {
   float x, y, z;
   bool leftClickPressed, rightClickPressed;
 
+
+  DT = (millis() - T)/1000.0;
+  T = millis();
+   K.F = {1.0,  0,
+		      0.0, 1.0};
+
+
+
+  
   buttonFunction(leftClickPressed, rightClickPressed);
   int scroll = EncoderValueFunction();
-  RotationSensor();
+  
   accelerometerFunction(x, y, z);
   //printInformation(leftClickPressed, rightClickPressed, x, y, z);
+  RotationSensor(x, y);
 
+
+  obs(0) = x;
+  obs(1) = y;
+  K.update(obs);
+
+  // Serial.println("Non-filtered values: ");
+  // Serial.print("X = ");
+  // Serial.println(x);
+  // Serial.print("Y = ");
+  // Serial.println(y, 6);
+
+  // Serial.println("Filtered values: ");
+  // Serial.print("X = ");
+  // Serial.println(K.x(0));
+  // Serial.print("Y = ");
+  // Serial.println(K.x(1));
+
+  x = K.x(0);
+  y = K.x(1);
+
+ x = curvedResponse(x);
+ y = curvedResponse(y);
+
+  // Serial.print("X: ");
+  // Serial.println(x);
+  // Serial.print("Y: ");
+  // Serial.println(y);
   mouseFunction(x, y, scroll);
 
-  delay(50);
+  delay(2);
 }
