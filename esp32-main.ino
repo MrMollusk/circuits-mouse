417b1399ad18e8664f1b7ff1bcae324eec8f8e6a
Seán Reilly <reillys9@tcd.ie>
2026-03-09 14:19:35 +0000

    Integrate touch sensor and Kalman filter
    Added touch sensor functionality and adjusted rotation sensor calculations.

--- esp32-main.ino
+++ esp32-main.ino
@@ -32,6 +32,7 @@ const int EncoderOne = 15;
 const int SCROLL = 35;
 const int LEFT_CLICK = 18;
 const int RIGHT_CLICK = 17;
+const int TOUCH_SENSOR = 34;
 
 //Global Variables
 byte range = 0x00;
@@ -41,6 +42,10 @@ int pastEncoderValue = 0;
 bool leftMouseCurrentlyBeingPressed = false;
 bool rightMouseCurrentlyBeingPressed = false;
 BleMouse bleMouse("AccelMouse", "ESP32", 100);
+
+double timeSinceTrigger = millis();
+double currentTime = millis();
+
 
 void setup() {
   Serial.begin(9600);
@@ -85,7 +90,7 @@ void setup() {
 /*
 K.R tells us the following values:
 
-K.H = {Measurement of the variance in the roll angle, The covariance of roll and pitch (should be zero as they are independent)
+K.R = {Measurement of the variance in the roll angle, The covariance of roll and pitch (should be zero as they are independent)
         Covariance of pitch and roll (should be the same as previous value), Variance of pitch}
 */
   K.R = {n_p*n_p,   0.0,
@@ -107,6 +112,10 @@ K.H = {Measurement of the variance in the roll angle, The covariance of roll and
   bleMouse.begin();   // pair from PC Bluetooth settings
 
   T = millis();
+
+  
+  double timeSinceTrigger = millis();
+  double currentTime = millis();
 }
 
 static inline int8_t clamp127(int v) { //This function just prevents the values from the accelerometer going over a certain value
@@ -160,8 +169,8 @@ void mouseFunction(float xVal, float yVal, int scroll) {
 
 void RotationSensor(float& x, float& y) {
   rotationVal = analogRead(SCROLL);
-  x = (x*0.25f/4095.0f)*rotationVal;
-  y = (y*0.25f/4095.0f)*rotationVal;
+  x = (x*0.5f/4095.0f)*rotationVal;
+  y = (y*0.5f/4095.0f)*rotationVal;
 }
 
 void printInformation(bool leftClickPressed, bool rightClickPressed, float x, float y, float z){
@@ -246,54 +255,70 @@ float curvedResponse(float input) {
   return scalingFactor*(exp(1.25f * a) - 1.0f);
 }
 
+
 void loop() {
+  
   float x, y, z;
   bool leftClickPressed, rightClickPressed;
 
 
-  DT = (millis() - T)/1000.0;
-  T = millis();
-   K.F = {1.0,  0,
-		      0.0, 1.0};
+  int val;
+  val = analogRead(TOUCH_SENSOR);
+  Serial.println(val, DEC);
+
+  currentTime = millis();
+
+  if(val > 10){
+    timeSinceTrigger = millis();
+  }
+
+  double dt = currentTime - timeSinceTrigger;
+
+  if(dt < 5000){
+    DT = (millis() - T)/1000.0;
+    T = millis();
+    K.F = {1.0,  0,
+            0.0, 1.0};
 
 
 
-  
-  buttonFunction(leftClickPressed, rightClickPressed);
-  int scroll = EncoderValueFunction();
-  
-  accelerometerFunction(x, y, z);
-  //printInformation(leftClickPressed, rightClickPressed, x, y, z);
-  RotationSensor(x, y);
+    
+    buttonFunction(leftClickPressed, rightClickPressed);
+    int scroll = EncoderValueFunction();
+    
+    accelerometerFunction(x, y, z);
+    //printInformation(leftClickPressed, rightClickPressed, x, y, z);
+    RotationSensor(x, y);
 
 
-  obs(0) = x;
-  obs(1) = y;
-  K.update(obs);
+    obs(0) = x;
+    obs(1) = y;
+    K.update(obs);
 
-  // Serial.println("Non-filtered values: ");
-  // Serial.print("X = ");
-  // Serial.println(x);
-  // Serial.print("Y = ");
-  // Serial.println(y, 6);
+    // Serial.println("Non-filtered values: ");
+    // Serial.print("X = ");
+    // Serial.println(x);
+    // Serial.print("Y = ");
+    // Serial.println(y, 6);
 
-  // Serial.println("Filtered values: ");
-  // Serial.print("X = ");
-  // Serial.println(K.x(0));
-  // Serial.print("Y = ");
-  // Serial.println(K.x(1));
+    // Serial.println("Filtered values: ");
+    // Serial.print("X = ");
+    // Serial.println(K.x(0));
+    // Serial.print("Y = ");
+    // Serial.println(K.x(1));
 
-  x = K.x(0);
-  y = K.x(1);
+    x = K.x(0);
+    y = K.x(1);
 
- x = curvedResponse(x);
- y = curvedResponse(y);
+  x = curvedResponse(x);
+  y = curvedResponse(y);
 
-  // Serial.print("X: ");
-  // Serial.println(x);
-  // Serial.print("Y: ");
-  // Serial.println(y);
-  mouseFunction(x, y, scroll);
+    // Serial.print("X: ");
+    // Serial.println(x);
+    // Serial.print("Y: ");
+    // Serial.println(y);
+    mouseFunction(x, y, scroll);
+  }
 
-  delay(2);
+  delay(100);
 }
