a5c212f1fa085997bd7d22600f301f0d7dc3b44a
Leon Coleman <lccoleman@protonmail.com>
2026-04-09 13:19:25 +0100

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,34 +1,39 @@
 #include <HijelHID_BLEMouse.h>
 
 HijelBLEMouse mouse("ESP32 Mouse", "ESP32");
-
 unsigned long lastPrint = 0;
 
 void setup() {
   Serial.begin(115200);
-  delay(200);
+  delay(500);
 
-  mouse.setLogLevel(HIDLogLevel::Verbose);
-  // mouse.clearBonds();   // use once, then comment out
+  mouse.setLogLevel(HIDLogLevel::Normal);
+
+  // Uncomment ONCE, flash, pair fresh, then comment it again
+  // mouse.clearBonds();
+
   mouse.begin();
+
+  Serial.println("BLE mouse started");
 }
 
 void loop() {
   if (millis() - lastPrint > 1000) {
     lastPrint = millis();
-    Serial.print("connected=");
+    Serial.print("bonded=");
+    Serial.print(mouse.isBonded());
+    Serial.print(" connected=");
     Serial.print(mouse.isConnected());
     Serial.print(" paired=");
     Serial.println(mouse.isPaired());
   }
 
-  if (!mouse.isPaired()) {
+  if (mouse.isPaired()) {
+    mouse.move(20, 0);
+    delay(1000);
+    mouse.move(-20, 0);
+    delay(1000);
+  } else {
     delay(20);
-    return;
   }
-
-  mouse.move(20, 0);
-  delay(1000);
-  mouse.move(-20, 0);
-  delay(1000);
 }
